069341722abe0324c89667001d22e9b8b9e2a6ec
Taui Silva <tauisilva@gmail.com>
2022-10-05 22:40:20 -0300

    Create HoraDoSistema.java

---
+++ HoraDoSistema.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class HoraDoSistema {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.print("A hora do sistema e: ");
+        System.out.println(relogio.toString());
+    }
+}
